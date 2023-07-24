6348e1ae8851794e425313c09f6c05b51bcc55a1
ZCha71 <140313843+ZCha71@users.noreply.github.com>
2023-07-24 12:11:27 +0400

    h
    n

---
+++ index.js
@@ -0,0 +1,101 @@
+function shineLinks(id) {
+  try {
+    var el = document.getElementById(id).getElementsByTagName("a");
+    var url = document.location.href;
+    for (var i = 0; i < el.length; i++) {
+      if (url == el[i].href) {
+        el[i].className += " act";
+      }
+    }
+  } catch (e) {}
+}
+class MobileNavbar {
+  constructor(mobileMenu, navList, navLinks) {
+    this.mobileMenu = document.querySelector(mobileMenu);
+    this.navList = document.querySelector(navList);
+    this.navLinks = document.querySelectorAll(navLinks);
+    this.activeClass = "active";
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  animateLinks() {
+    this.navLinks.forEach((link, index) => {
+      link.style.animation
+        ? (link.style.animation = "")
+        : (link.style.animation = `navLinkFade 0.5s ease forwards ${
+            index / 7 + 0.3
+          }s`);
+    });
+  }
+
+  handleClick() {
+    this.navList.classList.toggle(this.activeClass);
+    this.mobileMenu.classList.toggle(this.activeClass);
+    this.animateLinks();
+  }
+
+  addClickEvent() {
+    this.mobileMenu.addEventListener("click", this.handleClick);
+  }
+
+  init() {
+    if (this.mobileMenu) {
+      this.addClickEvent();
+    }
+    return this;
+  }
+}
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function (event) {
+  if (!event.target.matches(".dropbtn")) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
+      }
+    }
+  }
+};
+const mobileNavbar = new MobileNavbar(
+  ".mobile-menu",
+  ".nav-list",
+  ".nav-list li"
+);
+mobileNavbar.init();
+//FAQ
+
+let toggles = document.getElementsByClassName("toggle");
+let contentDiv = document.getElementsByClassName("content");
+let icons = document.getElementsByClassName("icon");
+
+for (let i = 0; i < toggles.length; i++) {
+  toggles[i].addEventListener("click", () => {
+    if (parseInt(contentDiv[i].style.height) != contentDiv[i].scrollHeight) {
+      contentDiv[i].style.height = contentDiv[i].scrollHeight + "px";
+      toggles[i].style.color = "#0084e9";
+      icons[i].classList.remove("fa-plus");
+      icons[i].classList.add("fa-minus");
+    } else {
+      contentDiv[i].style.height = "0px";
+      toggles[i].style.color = "#111130";
+      icons[i].classList.remove("fa-minus");
+      icons[i].classList.add("fa-plus");
+    }
+
+    for (let j = 0; j < contentDiv.length; j++) {
+      if (j !== i) {
+        contentDiv[j].style.height = "0px";
+        toggles[j].style.color = "#111130";
+        icons[j].classList.remove("fa-minus");
+        icons[j].classList.add("fa-plus");
+      }
+    }
+  });
+}
